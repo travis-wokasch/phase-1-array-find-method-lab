7bd135434ee6989065d99357d015011dc13fc753
Travis <tjwokasch@gmail.com>
2021-07-18 09:42:21 -0500

    commit tree ea73312bec0d8981fe675d8c5c14852c898d6c42

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+//write function that receives one argument as an array of Javascript Object
+//Object has two properties, year and result
+//Use .find to test the object for result = W
+//return year that W occurred
 
+function superbowlWin(kcChiefs){
+    let winYear = kcChiefs.find(superbowl => superbowl.result=== 'W')
+    if (winYear){
+        return winYear.year}
+}
